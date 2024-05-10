61e5b97160e3b866a3cf925d58b043dc7a90f4a9
Markiyan Hordiyenko <36715272+mhordiyenko@users.noreply.github.com>
2024-05-10 17:29:02 +0300

    upd train.py

--- src/model/train.py
+++ src/model/train.py
@@ -4,15 +4,19 @@ import argparse
 import glob
 import os
 
+import numpy as np
 import pandas as pd
+import mlflow
+
 
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.metrics import roc_auc_score, roc_curve
+from sklearn.model_selection import train_test_split
 
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -21,7 +25,8 @@ def main(args):
     X_train, X_test, y_train, y_test = split_data(df)
 
     # train model
-    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+    model = train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+
 
 
 def get_csvs_df(path):
@@ -34,11 +39,14 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
+def split_data(df):
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    return train_test_split(X, y, test_size=0.30, random_state=0)
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    return LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
 
 
 def parse_args():
